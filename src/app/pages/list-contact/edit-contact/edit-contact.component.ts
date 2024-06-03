7b14c1464d72d59839b6a710f4de3ee969dc53c5
artyomchch <artyomlos@yahoo.com>
2024-06-03 23:03:30 +0300

    1

--- src/app/pages/list-contact/edit-contact/edit-contact.component.ts
+++ src/app/pages/list-contact/edit-contact/edit-contact.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UsersService } from '../../../services/users.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-edit-contact',
@@ -9,16 +12,59 @@ import { UsersService } from '../../../services/users.service';
 })
 export class EditContactComponent implements OnInit {
 
-  
+  public form: FormGroup;
+  private userId: number;
+  private nameControl: FormControl
 
   constructor(
     private route: ActivatedRoute,
-    private usersService: UsersService
+    private router: Router,
+    private usersService: UsersService,
+    private fb: FormBuilder
   ) 
   { }
 
   ngOnInit(): void {
+    this.initUserId();
+    this.initForm();
+ 
+
     this.route.params.subscribe( ({id}) => console.log(this.usersService.getUser(+id)) )
+    console.log(this._clientAge?.value)
   }
+
+  
+  
+
+  initUserId(){
+    this.route.params.subscribe(({id}) => this.userId = +id)
+  }
+
+  initForm(){
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      phone: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.minLength(4)]],
+      website: ['', [Validators.required, Validators.minLength(4)]]
+    });
+    
+    this.form.patchValue(this.usersService.getUser(this.userId))
+  }
+
+  get _client() {
+    return this.form.get('username');
+}
+
+get _clientAge() {
+    return this.form.get('phone');
+}
+
+
+  updateUser(){
+    this.usersService.updateUser(this.userId, this.form.value)
+    this.router.navigate(['list-contact'])
+  }
+
+
 
 }
